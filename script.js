45d5398519577c85734769d48ce5c9eb14d3c8e5
vmqxdv <vquintilio@gmail.com>
2025-02-24 17:39:30 +0100

    fix: convertita variabile a costante e corretto calcolo sconto

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const travelLenght = Number(prompt('Quanti km devi percorrere?'));
 const userAge = Number(prompt('Quanti anni hai?'));
 
-let travelPriceRaw;
 const discountCalc = 
-userAge < 18 ? 0.2 :
-userAge > 65 ? 0.4 :
+userAge < 18 ? 0.8 :
+userAge > 65 ? 0.6 :
 1
+
+const travelPriceRaw = travelLenght * 0.21;
