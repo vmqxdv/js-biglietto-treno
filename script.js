50eeb4a67616b2fc4a998ba74887c2b18077b0e7
vmqxdv <vquintilio@gmail.com>
2025-02-24 18:38:48 +0100

    feat: Aggiunta funzione di controllo per userAge; fix: Corretto messaggi di errore

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ try {
   const travelLenght = Number(prompt('Quanti km devi percorrere?'));
   if (Math.sign(travelLenght) === -1 || isNaN(travelLenght)) {
     alert(`"${travelLenght}" non è un valore valido. Perfavore inserisci un numero positivo.`);
-    throw new Error(`Input "${travelLenght}" non valido: Numero negativo-15`);
+    throw new Error(`[travelLenght] Input "${travelLenght}" non valido`);
   };
   
-  let userAge = Number(prompt('Quanti anni hai?'));
-  
+  const userAge = Number(prompt('Quanti anni hai?'));
+  if (Math.sign(userAge) === -1 || isNaN(userAge) || userAge > 200) {
+    alert(`"${travelLenght}" non è un valore valido. Perfavore inserisci un numero positivo, e realistico.`);
+    throw new Error(`[userAge] Input "${travelLenght}" non valido`);
+  };
+
   const discountCalc = 
   userAge < 18 ? 0.8 :
   userAge > 65 ? 0.6 :
