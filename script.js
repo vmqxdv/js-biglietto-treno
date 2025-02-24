c8e0b456e719170b7bc89908efb25136cd1d485f
vmqxdv <vquintilio@gmail.com>
2025-02-24 17:35:19 +0100

    add: Creato calcolo sconto in base a età

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const travelLenght = prompt('Quanti km devi percorrere?');
-const userAge = prompt('Quanti anni hai?');
+const travelLenght = Number(prompt('Quanti km devi percorrere?'));
+const userAge = Number(prompt('Quanti anni hai?'));
+
+let travelPriceRaw;
+const discountCalc = 
+userAge < 18 ? 0.2 :
+userAge > 65 ? 0.4 :
+1
