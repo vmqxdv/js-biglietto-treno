a0d345a5a4c225a726a9f7f7097e2823bd3fb007
vmqxdv <vquintilio@gmail.com>
2025-02-24 17:46:00 +0100

    add: Calcolo, arrotondamento e alert del prezzo finale

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ userAge > 65 ? 0.6 :
 1;
 
 const travelPriceRaw = travelLenght * 0.21;
+const travelPriceFinal = roundToTwo(travelPriceRaw * discountCalc);
+
+alert(`Il prezzo del tuo biglietto è di €${travelPriceFinal}`);
+
+function roundToTwo(num) {
+  return parseFloat(num.toFixed(2)); 
+};
